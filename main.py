37b68e8e8b4f4fd25e1219d7ba73e201b0ce0a38
cristi <scarlat.cristian.gabriel@gmail.com>
2023-10-24 19:17:02 +0300

    added methods for each of quiz functionalities

---
+++ main.py
@@ -0,0 +1,16 @@
+from data import question_data
+from question_model import Question
+from quiz_brain import QuizBrain
+
+question_bank = []
+
+for question in question_data:
+    new_question = Question(question["text"], question["answer"])
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_question():
+    quiz.next_question()
+
+quiz.show_final_score()
