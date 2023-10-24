c5a45dd689daee2e046c49a058d7704652a05e6e
cristi <scarlat.cristian.gabriel@gmail.com>
2023-10-24 19:33:08 +0300

    change data

--- data.py
+++ data.py
@@ -1,14 +1,102 @@
 question_data = [
-{"text": "A slug's blood is green.", "answer": "True"},
-{"text": "The loudest animal is the African Elephant.", "answer": "False"},
-{"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
-{"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
-{"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", "answer": "True"},
-{"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.", "answer": "False"},
-{"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
-{"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
-{"text": "Google was originally called 'Backrub'.", "answer": "True"},
-{"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
-{"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
-{"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+    {
+        "category": "Entertainment: Film",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The movie 'The Nightmare before Christmas' was all done with physical objects.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+                "False"
+        ]
+    },
+    {
+        "category": "Entertainment: Film",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Matt Damon played an astronaut stranded on an extraterrestrial planet in both of the movies Interstellar and The Martian.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+                "False"
+        ]
+    },
+    {
+        "category": "Entertainment: Film",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The 2010 film 'The Social Network' is a biographical drama film about MySpace founder Tom Anderson.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+                "True"
+        ]
+    },
+    {
+        "category": "Entertainment: Film",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The word 'Inception' came from the 2010 blockbuster hit 'Inception'.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+                "True"
+        ]
+    },
+    {
+        "category": "Entertainment: Film",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Ewan McGregor did not know the name of the second prequel film of Star Wars during and after filming.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+                "False"
+        ]
+    },
+    {
+        "category": "Entertainment: Film",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "In the original Star Wars trilogy, David Prowse was the actor who physically portrayed Darth Vader.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+                "False"
+        ]
+    },
+    {
+        "category": "Entertainment: Film",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "In the movie The Revenant, DiCaprio&#039;s character hunts down the killer of his son.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+                "False"
+        ]
+    },
+    {
+        "category": "Entertainment: Film",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Brandon Routh plays the titular character in the movie 'John Wick'.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+                "True"
+        ]
+    },
+    {
+        "category": "Entertainment: Film",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "George Lucas directed the entire original Star Wars trilogy.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+                "True"
+        ]
+    },
+    {
+        "category": "Entertainment: Film",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "'Foodfight!' earned less than $80,000 at box office.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+                "False"
+        ]
+    }
 ]
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from quiz_brain import QuizBrain
 question_bank = []
 
 for question in question_data:
-    new_question = Question(question["text"], question["answer"])
+    new_question = Question(question["question"], question["correct_answer"])
     question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
