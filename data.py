fa490c55087770e48e752eef6760cca33290ad24
cristi <scarlat.cristian.gabriel@gmail.com>
2023-11-12 16:55:56 +0200

    API trivia call

--- data.py
+++ data.py
@@ -1,102 +1,11 @@
-question_data = [
-    {
-        "category": "Entertainment: Film",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "The movie 'The Nightmare before Christmas' was all done with physical objects.",
-        "correct_answer": "True",
-        "incorrect_answers": [
-                "False"
-        ]
-    },
-    {
-        "category": "Entertainment: Film",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "Matt Damon played an astronaut stranded on an extraterrestrial planet in both of the movies Interstellar and The Martian.",
-        "correct_answer": "True",
-        "incorrect_answers": [
-                "False"
-        ]
-    },
-    {
-        "category": "Entertainment: Film",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "The 2010 film 'The Social Network' is a biographical drama film about MySpace founder Tom Anderson.",
-        "correct_answer": "False",
-        "incorrect_answers": [
-                "True"
-        ]
-    },
-    {
-        "category": "Entertainment: Film",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "The word 'Inception' came from the 2010 blockbuster hit 'Inception'.",
-        "correct_answer": "False",
-        "incorrect_answers": [
-                "True"
-        ]
-    },
-    {
-        "category": "Entertainment: Film",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "Ewan McGregor did not know the name of the second prequel film of Star Wars during and after filming.",
-        "correct_answer": "True",
-        "incorrect_answers": [
-                "False"
-        ]
-    },
-    {
-        "category": "Entertainment: Film",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "In the original Star Wars trilogy, David Prowse was the actor who physically portrayed Darth Vader.",
-        "correct_answer": "True",
-        "incorrect_answers": [
-                "False"
-        ]
-    },
-    {
-        "category": "Entertainment: Film",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "In the movie The Revenant, DiCaprio&#039;s character hunts down the killer of his son.",
-        "correct_answer": "True",
-        "incorrect_answers": [
-                "False"
-        ]
-    },
-    {
-        "category": "Entertainment: Film",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "Brandon Routh plays the titular character in the movie 'John Wick'.",
-        "correct_answer": "False",
-        "incorrect_answers": [
-                "True"
-        ]
-    },
-    {
-        "category": "Entertainment: Film",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "George Lucas directed the entire original Star Wars trilogy.",
-        "correct_answer": "False",
-        "incorrect_answers": [
-                "True"
-        ]
-    },
-    {
-        "category": "Entertainment: Film",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "'Foodfight!' earned less than $80,000 at box office.",
-        "correct_answer": "True",
-        "incorrect_answers": [
-                "False"
-        ]
-    }
-]
+import requests
+
+parameters = {
+    "amount": 10,
+    "category": 15,
+    "type": "boolean"
+}
+
+res = requests.get('https://opentdb.com/api.php', params=parameters)
+res.raise_for_status()
+question_data = res.json()["results"]
